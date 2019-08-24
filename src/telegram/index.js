298a0f691cb2ca356d538c51a6056405f96598a3
nikitarudy <rudiynikita@gmail.com>
2019-08-24 11:09:42 +0300

    stub heroku default port

--- src/telegram/index.js
+++ src/telegram/index.js
@@ -1,3 +1,5 @@
+const http = require('http')
+
 const handlers = require('./handlers')
 const connectToDb = require('../db')
 const telegramBot = require('./bot')
@@ -9,3 +11,11 @@ const telegramBot = require('./bot')
   telegramBot.command('subscribe', handlers.subscribe)
   telegramBot.launch()
 })()
+
+const server = http.createServer((rqe, res) => {
+  res
+    .writeHead(200, { 'Content-type': 'text/html' })
+    .end(`<h1><a href="https://t.me/new_listings_bot">Telegram bot</a></h1>`)
+})
+
+server.listen(process.env.PORT)
